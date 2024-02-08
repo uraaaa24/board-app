afe652c3bd3a3f06df5f4d7f1ef31c1d7bdb2a05
Yuto Urashima <yuto.urashima24@gmail.com>
2024-02-08 21:15:15 +0900

    change directory structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
-import { Button } from '@/components/ui/button'
+import { UserButton } from '@clerk/nextjs'
 
 export default function Home() {
-  return <Button size="sm">Click me!</Button>
+  return (
+    <div className="flex flex-col gap-y-4">
+      <div>This is a screen fot Authenticated users only</div>
+      <div>
+        <UserButton />
+      </div>
+    </div>
+  )
 }
