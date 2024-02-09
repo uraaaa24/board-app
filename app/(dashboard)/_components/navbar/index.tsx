d4337f98c36f7d1d4609b817509a946fcff127cb
Yuto Urashima <yuto.urashima24@gmail.com>
2024-02-09 16:31:22 +0900

    Add serch area

--- app/(dashboard)/_components/navbar/index.tsx
+++ app/(dashboard)/_components/navbar/index.tsx
@@ -3,7 +3,7 @@ import { UserButton } from '@clerk/nextjs'
 const Navbar = () => {
   return (
     <div className="flex items-center gap-x-4 p-5 bg-green-500">
-      <div className="hidden lg:flex lg;flex-1">{/* TODO: Add search */}</div>
+      <div className="hidden lg:flex lg:flex-1 bg-yellow-500">Search</div>
       <UserButton />
     </div>
   )
