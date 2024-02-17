91ac10abb216b0c496819a139a6c684f86488aa1
Yuto Urashima <yuto.urashima24@gmail.com>
2024-02-17 18:17:56 +0900

    Create Rename action component

--- components/common/modals/renameModal/index.tsx
+++ components/common/modals/renameModal/index.tsx
@@ -1,10 +1,48 @@
 'use client'
 
-import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from '@/components/ui/dialog'
+import { FormEventHandler, useEffect, useState } from 'react'
+
+import { Button } from '@/components/ui/button'
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle
+} from '@/components/ui/dialog'
+import { Input } from '@/components/ui/input'
+import { api } from '@/convex/_generated/api'
+import useApiMutation from '@/hooks/useApiMutaion'
 import { useRenameModal } from '@/store/useRenameModal'
+import { toast } from 'sonner'
 
 const RenameModal = () => {
+  const { mutate, pending } = useApiMutation(api.board.update)
   const { initialValues, isOpen, onClose } = useRenameModal()
+
+  const [title, setTitle] = useState(initialValues.title)
+
+  useEffect(() => {
+    setTitle(initialValues.title)
+  }, [initialValues.title])
+
+  const onSubmit: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault()
+
+    mutate({
+      id: initialValues.id,
+      title
+    })
+      .then(() => {
+        toast.success('Board renamed!')
+        onClose()
+      })
+      .catch(() => {
+        toast.error('Failed to rename board')
+      })
+  }
 
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
@@ -13,6 +51,28 @@ const RenameModal = () => {
           <DialogTitle>Edit board title</DialogTitle>
         </DialogHeader>
         <DialogDescription>Enter a new title for this board</DialogDescription>
+        <form onSubmit={onSubmit} className="space-y-4">
+          <Input
+            disabled={pending}
+            required
+            maxLength={60}
+            value={title}
+            onChange={(e) => {
+              setTitle(e.target.value)
+            }}
+            placeholder="Board title"
+          />
+          <DialogFooter>
+            <DialogClose asChild>
+              <Button type="button" variant="outline">
+                Cancel
+              </Button>
+            </DialogClose>
+            <Button disabled={pending} type="submit">
+              Save
+            </Button>
+          </DialogFooter>
+        </form>
       </DialogContent>
     </Dialog>
   )
