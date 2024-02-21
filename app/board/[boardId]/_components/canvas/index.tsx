28ddf721b4a6ab21a9b98c1fe21fdf8bb19fd76e
Yuto Urashima <yuto.urashima24@gmail.com>
2024-02-21 20:54:07 +0900

    Setup of liveblocks

--- app/board/[boardId]/_components/canvas/index.tsx
+++ app/board/[boardId]/_components/canvas/index.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { useSelf } from '@/liveblocks.config'
 import Information from '../information'
 import Participants from '../participants.tsx'
 import Toolbar from '../toolbar'
@@ -9,6 +10,10 @@ interface CanvasProps {
 }
 
 const Canvas = ({ boardId }: CanvasProps) => {
+  const info = useSelf((me) => me.info)
+
+  console.log(info)
+
   return (
     <main className="h-full w-full relative bg-neutral-100 touch-none">
       <Information />
